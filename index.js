2780a986fc33fe41d20fd5c0adef37bad3041500
Patrick Ly <patrickly72@gmail.com>
2021-04-14 15:39:28 -0700

    added prompts for inquire

--- index.js
+++ index.js
@@ -1,2 +1,55 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+inquirer.prompt([
+    {
+      type: 'input',
+      message: 'what is the title of your project',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'what is the description of your project',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Instructions to install',
+      name: 'instructions',
+    },
+    
+    {
+        type: 'input',
+        message: 'How to use your project',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Who contributed to your project?',
+        name: 'contributing',
+      },
+      {
+        type: 'input',
+        message: 'How to test your project?',
+        name: 'test',
+      },
+
+      {
+        type: 'checkbox',
+        message: 'Choose a license',
+        choices: ["Apache License 2.0", "MIT", "ISC", "GNU"],
+      },
+
+      {
+        type: 'input',
+        message: 'whats your email address?',
+        name: 'email',
+      },
+
+      {
+        type: 'input',
+        message: 'whats your GitHub?',
+        name: 'github',
+      },
+    
+  ])
